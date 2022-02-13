a9ae14d4b9e2194c9f962ee794e712123551c3af
halil180 <halilkom180@.com>
2022-02-13 21:25:15 +0100

    week 1 assignments

---
+++ week1/calculator/index.js
@@ -0,0 +1,7 @@
+////import functions 
+const calculate = require('./add')
+////test results
+console.log(calculate.Add(2,3)) /// 2+3
+console.log(calculate.Subtract(1,3))/// 1-3
+console.log(calculate.Multiply(5,3)) ////5 * 3
+console.log(calculate.Divide(6,3))/// 6 /3
